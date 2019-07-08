68fd0c55b829dfc6a74184201ef864f08294d215
Dmitry Yan <safedima@protonmail.com>
2019-07-08 20:07:35 +0400

    Update after 5 years, and rewrite for GNU/Linux support :feelsgood:

--- iso2usb.py
+++ iso2usb.py
@@ -1,17 +1,56 @@
 import os, sys
 
-if len(sys.argv) != 2:
-	sys.exit('Usage: sudo %s path-to-iso' % sys.argv[0])
+if len(sys.argv) != 3:
+	sys.exit('Usage: sudo %s iso-file burn-device' % sys.argv[0])
 
-if not os.path.exists(sys.argv[1]):
-    sys.exit('ERROR: ISO file %s was not found!' % sys.argv[1])
+isofile = sys.argv[1]
+device = sys.argv[2]
 
-print 'Locate the disk identifier in the following list:'
+if not os.path.exists(isofile):
+	sys.exit('ERROR: ISO file %s was not found!' % isofile)
 
-f = os.popen('diskutil list')
+if not os.path.exists(device):
+	sys.exit('ERROR: Device on %s was not found!' % device)
+
+print 'Starting ISO Flashburner...'
+print ''
+
+f = os.popen('sudo fdisk -l %s' % device)
 print f.read()
 
-diskid = raw_input("Type the disk identifier (Something like disk1 or disk1s1): ")
+thisdriveornot = raw_input("This is really your burning drive? [y/n]: ")
+
+if thisdriveornot != 'y':
+	sys.exit('Okay. Terminating flashburner..')
+
+hybridornot = raw_input("Convert ISO file to hybrid? (Try if simple burn don't working) [y/n]: ")
+
+if hybridornot != 'n':
+	copyornot = raw_input("Copy ISO before converting? (recommended but it's maybe very long) [y/n]: ")
+	if copyornot != 'n':
+		# also code..
+
+if hybridornot != 'y':
+	print('Unmounting device %s..' % device)
+	os.system('sudo umount -f %s' % device)
+
+	print ''
+	wannacnt = raw_input("You really wanna continue and burn your flash drive? (WARNING: It's full reformating! Backup your data immediately!) [y/n]: ")	
+	
+	if wannacnt != 'y':
+		print('Mounting back %s on /mnt/flash/' % device)
+		os.system("sudo mount -o rw,force %s /mnt/flash" % device)
+		sys.exit('Terminating...')
+
+	print('Burning on %s..' % device)
+	os.system("sudo dd if=%s of=%s status=progress" % (isofile, device))
+
+print 'Done!'
+
+sys.exit()
+
+os.system("sudo dd if=")
+
 
 os.system('hdiutil convert -format UDRW -o bootable.img %s' % sys.argv[1])
 os.system('mv bootable.img.dmg bootable.img')
